f780dcbc8134b3205d671e1bc01fdc2b59bf1f98
Appletaart <papayahanaga@gmail.com>
2020-08-25 12:58:41 +0200

    first commit

--- admin/includes/dbc.php
+++ admin/includes/dbc.php
@@ -1,10 +1,5 @@
 <?php
 
-/*define('DB_HOST', '188.121.44.76:3306');
-define('DB_USER', 'applebar');
-define('DB_PASS', 'Kouf43~7');
-define('DB_NAME', 'applebar');*/
-
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','root');
